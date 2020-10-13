b3b0d328a0177ff8b5c561ca5a897be67f7274a7
DanielMeixner <daniel.meixner@live.com>
2020-10-13 13:36:26 +0200

    V4

--- notebooks/dmxnotebook.py
+++ notebooks/dmxnotebook.py
@@ -7,3 +7,14 @@ blob_sas_token = r"?st=2019-02-26T02%3A34%3A32Z&se=2119-02-27T02%3A34%3A00Z&sp=r
 wasbs_path = 'wasbs://%s@%s.blob.core.windows.net/%s' % (blob_container_name, blob_account_name, blob_relative_path)
 spark.conf.set('fs.azure.sas.%s.%s.blob.core.windows.net' % (blob_container_name, blob_account_name), blob_sas_token)
 print('Remote blob path: ' + wasbs_path)
+
+# COMMAND ----------
+
+df = spark.read.parquet(wasbs_path)
+print('Register the DataFrame as a SQL temporary view: source')
+df.createOrReplaceTempView('source')
+
+# COMMAND ----------
+
+print('Displaying top 10 rows: ')
+display(spark.sql('SELECT * FROM source LIMIT 10'))
